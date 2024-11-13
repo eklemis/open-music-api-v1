1af42ebaa4d1546a4d62b02dd51581c1fe97fdd9
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-13 11:25:16 +0800

    prepared AlbumHAndler constructor

---
+++ src/api/albums/handler.js
@@ -0,0 +1,12 @@
+class AlbumHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.getAlbumsHandler = this.getAlbumsHandler.bind(this);
+    this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+    this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+    this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+  }
+}
