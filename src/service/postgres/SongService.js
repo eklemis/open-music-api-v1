318679931c505b3ed17f292870d72ee745fb37cd
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-14 10:49:52 +0800

    completed first version of SongService

--- src/service/postgres/SongService.js
+++ src/service/postgres/SongService.js
@@ -5,11 +5,64 @@ class SongService {
   constructor() {
     this._pool = new Pool();
   }
-  async getSongs() {}
-  async getSongById(id) {}
-  async addSong({ title, year, genre, performer, duration, albumId }) {}
-  async changeSong(id, { title, year, genre, performer, duration, albumId }) {}
-  async deleteSongById(id) {}
+  async getSongs() {
+    const result = await this._pool.query("SELECT * FROM songs");
+    return result.rows;
+  }
+  async getSongById(id) {
+    const query = {
+      text: "SELECT * FROM songs WHERE id=$1",
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+
+    return result.rows[0];
+  }
+  async addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: "INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
+      values: [
+        albumId,
+        title,
+        year,
+        genre,
+        performer,
+        duration,
+        createdAt,
+        updatedAt,
+      ],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows[0].id;
+  }
+  async changeSong(id, { title, year, genre, performer, duration, albumId }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer=$4, duration=$5, updated_at=$6, updated_at = $7 WHERE id = $8 RETURNING id",
+      values: [title, year, genre, performer, duration, albumId, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new Error("Gagal memperbarui catatan. Id tidak ditemukan");
+    }
+  }
+  async deleteSongById(id) {
+    const query = {
+      text: "DELETE FROM songs WHERE id = $1 RETURNING id",
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new Error("Catatan gagal dihapus. Id tidak ditemukan");
+    }
+  }
 }
 
 module.exports = SongService;
