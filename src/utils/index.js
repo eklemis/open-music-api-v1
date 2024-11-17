6803060cd9550ef24e5e42a8902a9e19beb9c082
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-18 05:50:00 +0800

    prepared data mapper for database to service model

---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+const mapDBAlbumToModel = ({ id, name, year, created_at, updated_at }) => ({
+  id,
+  name,
+  year,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+const mapDBSongToModel = ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  album_id,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+module.exports = { mapDBAlbumToModel, mapDBSongToModel };
