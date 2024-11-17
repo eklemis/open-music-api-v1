fb2a8eea27cc5757c4ef24405c2268324198cb6d
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-18 06:15:27 +0800

    completed first version of album plugin's handler

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -9,10 +9,58 @@ class AlbumHandler {
     this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
     this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
   }
-  async getAlbumsHandler() {}
-  async getAlbumByIdHandler(request, h) {}
-  async postAlbumHandler(request, h) {}
-  async putAlbumByIdHandler(request, h) {}
-  async deleteAlbumByIdHandler(request, h) {}
+  async getAlbumsHandler() {
+    const albums = await this._service.getAlbums();
+    return {
+      status: "success",
+      data: {
+        albums,
+      },
+    };
+  }
+  async getAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+    const album = await this._service.getAlbumById(id);
+    return {
+      status: "success",
+      data: {
+        album,
+      },
+    };
+  }
+  async postAlbumHandler(request, h) {
+    const { name, year } = request.payload;
+
+    const albumId = await this._service.addAlbum({ name, year });
+
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  async putAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+
+    await this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: "success",
+      message: "Catatan berhasil diperbarui",
+    };
+  }
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._service.deleteAlbumById(id);
+
+    return {
+      status: "success",
+      message: "Album berhasil dihapus",
+    };
+  }
 }
 module.exports = AlbumHandler;
