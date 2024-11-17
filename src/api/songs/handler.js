cb9c8ed7f39bd6df758331c272ecf6a7d769599f
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-18 06:24:35 +0800

    completed first version of song plugin's handler

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -3,10 +3,72 @@ class SongHandler {
     this._service = service;
     this._validator = validator;
   }
-  async getSongsHandler() {}
-  async getSongByIdHandler(request, h) {}
-  async postSongHandler(request, h) {}
-  async putSongByIdHandler(request, h) {}
-  async deleteSongByIdHandler(request, h) {}
+  async getSongsHandler() {
+    const songs = await this._service.getSongs();
+    return {
+      status: "success",
+      data: {
+        songs,
+      },
+    };
+  }
+  async getSongByIdHandler(request, h) {
+    const { id } = request.params;
+    const song = await this._service.getSongById(id);
+    return {
+      status: "success",
+      data: {
+        song,
+      },
+    };
+  }
+  async postSongHandler(request, h) {
+    const {
+      title = "untitled",
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    } = request.payload;
+
+    const songId = await this._service.addSong({
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    });
+
+    const response = h.response({
+      status: "success",
+      message: "Lagu berhasil ditambahkan",
+      data: {
+        songId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  async putSongByIdHandler(request, h) {
+    const { id } = request.params;
+
+    await this._service.changeSongById(id, request.payload);
+
+    return {
+      status: "success",
+      message: "Catatan berhasil diperbarui",
+    };
+  }
+  async deleteSongByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._service.deleteSongById(id);
+
+    return {
+      status: "success",
+      message: "Catatan berhasil dihapus",
+    };
+  }
 }
 module.exports = SongHandler;
