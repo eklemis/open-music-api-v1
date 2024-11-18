7cf39c4f16e4d0b3365598062ef4ba42ed34e6c2
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-18 10:21:46 +0800

    registered album service to server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+const albums = require("./api/albums");
+const AlbumServices = require("./services/postgres/AlbumServices");
 
 const init = async () => {
+  const albumServices = new AlbumServices();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +15,15 @@ const init = async () => {
       },
     },
   });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumServices,
+      //validator: NotesValidator,
+    },
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
