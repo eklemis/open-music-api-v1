88d2978e1e0020ae047268a18a0a6efbe3e28d5a
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-11-18 17:22:44 +0800

    completed first working version of Songs plugin

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -41,7 +41,10 @@ class SongsService {
     const result = await this._pool.query(query);
     return result.rows[0].id;
   }
-  async changeSong(id, { title, year, genre, performer, duration, albumId }) {
+  async changeSongById(
+    id,
+    { title, year, genre, performer, duration, albumId },
+  ) {
     const updatedAt = new Date().toISOString();
     const query = {
       text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer=$4, duration=$5, album_id=$6, updated_at = $7 WHERE id = $8 RETURNING id",
